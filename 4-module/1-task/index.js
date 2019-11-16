706abc82c0f6eb9950ccd6e3c5e649a196d088f3
aldrapov <aldrapov@gmail.com>
2019-11-17 01:30:59 +0700

    task-4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,11 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  const ul = document.createElement('ul');
+  for (let i = 0; i < friends.length; i += 1) {
+    const li = document.createElement('li');
+    li.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+    ul.append(li);
+  }
+  return ul;
 }
