ff4a594e0807f66624b307255f37b40a5acd18b3
aldrapov <aldrapov@gmail.com>
2019-11-17 17:18:15 +0700

    5-task

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,33 @@
  * @param {Element} table
  */
 function highlight(table) {
+  const tbody = table.querySelector('tbody');
+  const tabRows = tbody.querySelectorAll('tr');
+
+  for (let i = 0; i < tabRows.length; i += 1) {
+    const tabCols = tabRows[i].querySelectorAll('td');
+
+    // Проставит класс available/unavailable, в зависимости
+    // от значения атрибута data-available у ячейки Status
+    // Проставит атрибут hidden, если атрибута data-available нет вообще
+    if ('available' in tabCols[3].dataset === false) {
+      tabRows[i].hidden = true;
+    } else if (tabCols[3].dataset.available === 'true') {
+      tabRows[i].classList.add('available');
+    } else {
+      tabRows[i].classList.add('unavailable');
+    }
+
+    // Проставит класс male/female, в зависимости от содержимого ячейки Gender
+    if (tabCols[2].innerHTML === 'm') {
+      tabRows[i].classList.add('male');
+    } else if (tabCols[2].innerHTML === 'f') {
+      tabRows[i].classList.add('female');
+    }
+
+    // Установит inline-стиль style="text-decoration: line-through", если значение ячейки Age меньше 18
+    if (+tabCols[1].innerHTML < 18) {
+      tabRows[i].style.textDecoration = 'line-through';
+    }
+  }
 }
